d62eca46ce64c52b46c1dc9bc21c3a74e18a8142
saowaluck <pop@prontomaarketing.com>
2018-12-08 14:45:22 +0700

    :recycle: change url to reverse

--- simplezat/ratings/tests.py
+++ simplezat/ratings/tests.py
@@ -1,8 +1,10 @@
+from django.urls import reverse
 from django.test import TestCase
 
 
 class RatingViewTest(TestCase):
     def test_reating_view_should_be_accessable(self):
-        response = self.client.get('/ratings/')
+        url = reverse('ratings')
+        response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
 
