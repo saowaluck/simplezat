8837e5e06e476ec5166fe99934a8b34267ec93d4
saowaluck <pop@prontomaarketing.com>
2018-12-08 12:19:03 +0700

    Add wait 100

--- tests/cypress/integration/simplezat/follow_up.spac.js
+++ tests/cypress/integration/simplezat/follow_up.spac.js
@@ -8,14 +8,16 @@ context('Rating', () => {
   it('should have 3 rating', () => {
     cy.contains('How do we do')
     cy.get('img').should('have.attr', 'alt', 'Positive')
-    cy.get('img').should('have.attr', 'alt', 'Netual')
+    cy.get('img').should('have.attr', 'alt', 'Neutral')
     cy.get('img').should('have.attr', 'alt', 'Negative')
 
     cy.get('img["Positive"]').click()
+    cy.wait(1000)
 
     cy.contains('Any comment?')
-    cy.get('inputp[name="comment"]').type('You are doing great!')
+    cy.get('inputp[name=comment"]').type('You are doing great!')
     cy.get('button').click()
+    cy.wait(1000)
 
     cy.contains('Thank you')
   })
